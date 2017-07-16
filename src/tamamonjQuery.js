e9cabb269870330dd2fbe1f7f50c776ed888a432
Miho Tse <mihotse@gmail.com>
2017-07-16 11:32:37 +0100

    Move feed and play logic out of html file

--- src/tamamonjQuery.js
+++ src/tamamonjQuery.js
@@ -8,15 +8,27 @@ $(document).ready(function() {
   updateEnergy();
   $("#energyMessage").text(tamamon.energyMessage);
 
-  $('#feed').on('click', function() {
+  $('#feed').on('click', function startAnimationFeed() {
     tamamon.feed();
+    $("#default01").prop('src', "Images/TamamonFeed02.png");
+    $("#default02").prop('src', "Images/TamamonFeed01.png");
+    setTimeout(function() {
+      $("#default01").prop('src', "Images/Tamamon01.png");
+      $("#default02").prop('src', "Images/Tamamon02.png");
+    }, 3000);
     tamamon.showEnergyErrors();
     updateEnergy();
     $('#energyMessage').text(tamamon.energyMessage);
   });
 
-  $('#play').on('click', function() {
+  $('#play').on('click', function startAnimationPlay() {
     tamamon.play();
+    $("#default01").prop('src', "Images/TamamonPlay01.png");
+    $("#default02").prop('src', "Images/TamamonPlay02.png");
+    setTimeout(function() {
+      $("#default01").prop('src', "Images/Tamamon01.png");
+      $("#default02").prop('src', "Images/Tamamon02.png");
+    }, 3000);
     tamamon.showEnergyErrors();
     updateEnergy();
     $('#energyMessage').text(tamamon.energyMessage);
